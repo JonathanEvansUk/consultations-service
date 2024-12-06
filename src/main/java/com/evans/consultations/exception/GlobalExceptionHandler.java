81baf4fae4651ea1aa6e391e99e66450f44683b6
Jonathan Evans <jonathan.evans.1994@gmail.com>
2024-12-06 18:03:52 +0000

    chore(logging): reduce noise from exception stacktrace being logged

--- src/main/java/com/evans/consultations/exception/GlobalExceptionHandler.java
+++ src/main/java/com/evans/consultations/exception/GlobalExceptionHandler.java
@@ -1,25 +1,27 @@
 package com.evans.consultations.exception;
 
 import com.evans.consultations.model.ErrorDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleException(Exception e) {
-        logger.error("Unexpected exception: {}", e);
+        log.error("Unexpected exception: ", e);
 
         return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred");
     }
 
     @ExceptionHandler(ConsultationsException.class)
     public ResponseEntity<ErrorDto> handleConsultationsException(ConsultationsException e) {
-        logger.warn("Consultations exception: {}", e);
+        log.error("Consultations exception: {}", e.getMessage());
 
         return createErrorResponse(e.getStatus(), e.getMessage());
     }
